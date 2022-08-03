65e624671b91f0cc57bb1ec442f49835fadcc96c
APBD98 <azraiputrabarumundaulay@gmail.com>
2022-08-03 21:00:50 +0700

    mengganti jquery dengan fetch api

--- script.js
+++ script.js
@@ -1,37 +1,93 @@
-$('.search-button').on('click', function () {
+// $('.search-button').on('click', function () {
   
-    $.ajax({
-    url: 'http://www.omdbapi.com/?apikey=ee7a4fa8&s=' + $('.input-keyword').val(),
-    success: results => {
-        const movies = results.Search;
-        let cards = '';
-        for (let i = 0; i < movies.length; i++) {
-            const m = movies[i];
-           cards += showCards(m);
+//     $.ajax({
+//     url: 'http://www.omdbapi.com/?apikey=ee7a4fa8&s=' + $('.input-keyword').val(),
+//     success: results => {
+//         console.log(results)
+//         const movies = results.Search;
+//         let cards = '';
+//         for (let i = 0; i < movies.length; i++) {
+//             const m = movies[i];
+//            cards += showCards(m);
             
-        }
+//         }
 
-        $('.movie-container').html(cards);
+//         $('.movie-container').html(cards);
 
-        $('.modal-details-button').on('click', function () {
-            console.log($(this).data("imdbid"));
-            $.ajax({
-                url: 'http://www.omdbapi.com/?apikey=ee7a4fa8&i=' + $(this).data("imdbid"),
-                success: m => {
-                    console.log(m);
-                    const movieDetails = showMovieDetails(m);
-                    $('.modal-body').html(movieDetails);
-                },
-                error: e => {
-                    console.log(e.responseText)
-                }
-            })
-        })
-    },
-    error: e => {
+//         $('.modal-details-button').on('click', function () {
+//             console.log($(this).data("imdbid"));
+//             $.ajax({
+//                 url: 'http://www.omdbapi.com/?apikey=ee7a4fa8&i=' + $(this).data("imdbid"),
+//                 success: m => {
+//                     console.log(m);
+//                     const movieDetails = showMovieDetails(m);
+//                     $('.modal-body').html(movieDetails);
+//                 },
+//                 error: e => {
+//                     console.log(e.responseText)
+//                 }
+//             })
+//         })
+//     },
+//     error: e => {
         
-    }
-})  
+//     }
+// })  
+// })
+
+
+const searchButton = document.getElementById('search-button');
+searchButton.addEventListener('click', function () {
+    const  inputKeyword = document.querySelector('.input-keyword').value;
+    const request = new Request("http://www.omdbapi.com/?apikey=ee7a4fa8&s=" + inputKeyword, {
+        method: "GET"
+
+    })
+
+    const response = fetch(request);
+
+    response
+        .then(response => response.json())
+        .then(result => {
+            const movies = result.Search;
+            let cards = '';
+
+            movies.forEach(m => {
+                cards += showCards(m);
+            });
+
+            document.querySelector('.movie-container').innerHTML = cards;
+
+            const detailButton = document.getElementsByClassName('modal-details-button');
+            
+            for (let i = 0; i < detailButton.length; i++) {
+                const e = detailButton[i];
+                e.addEventListener('click', function () {
+                    const imdbid = this.dataset.imdbid;
+                    const request = new Request('http://www.omdbapi.com/?apikey=ee7a4fa8&i=' + imdbid, {
+                        method: "GET"
+                    })
+
+                    const response = fetch(request);
+
+                    response
+                        .then(response => response.json())
+                        .then(m => {
+                        const movieDetails = showMovieDetails(m);
+                        document.querySelector('.modal-body').innerHTML = movieDetails;
+                        })
+                        .catch(error => error.status)
+
+                })
+                
+            }
+            
+        })
+
+        .catch(error => error.status)
+        
+
+    
 })
 
 
